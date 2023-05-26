efd70f7c1c79a69ec3b5ffcc2e7a93e62d8749b3
mbcordeiro <matheusdebarroscordeiro@gmail.com>
2023-05-26 00:37:13 -0300

    first commit

---
+++ src/main/java/com/matheuscordeiro/creditcardbilljob/CreditCardBillJobApplication.java
@@ -0,0 +1,13 @@
+package com.matheuscordeiro.creditcardbilljob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreditCardBillJobApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreditCardBillJobApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/matheuscordeiro/creditcardbilljob/CreditCardBillJobApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matheuscordeiro.creditcardbilljob;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreditCardBillJobApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
