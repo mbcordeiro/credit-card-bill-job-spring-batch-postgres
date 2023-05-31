4085b32ac31fc8b9f61bd6bffc35202854bbbca3
mbcordeiro <matheusdebarroscordeiro@gmail.com>
2023-05-30 22:43:46 -0300

    Credit card bill

--- src/main/java/com/matheuscordeiro/creditcardbilljob/domain/CreditCard.java
+++ src/main/java/com/matheuscordeiro/creditcardbilljob/domain/CreditCard.java
@@ -3,4 +3,17 @@ package com.matheuscordeiro.creditcardbilljob.domain;
 public class CreditCard {
 	private int creditCardNumber;
 	private Customer customer;
+	
+	public int getCreditCardNumber() {
+		return creditCardNumber;
+	}
+	public void setCreditCardNumber(int creditCardNumber) {
+		this.creditCardNumber = creditCardNumber;
+	}
+	public Customer getCustomer() {
+		return customer;
+	}
+	public void setCustomer(Customer customer) {
+		this.customer = customer;
+	}
 }
---
+++ src/main/java/com/matheuscordeiro/creditcardbilljob/domain/CreditCardBill.java
@@ -0,0 +1,31 @@
+package com.matheuscordeiro.creditcardbilljob.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CreditCardBill {
+	private Customer customer;
+	private CreditCard creditCard;
+	private List<Transaction> transactions = new ArrayList<>();
+	
+	public Customer getCustomer() {
+		return customer;
+	}
+	public void setCustomer(Customer customer) {
+		this.customer = customer;
+	}
+	public CreditCard getCreditCard() {
+		return creditCard;
+	}
+	public void setCreditCard(CreditCard creditCard) {
+		this.creditCard = creditCard;
+	}
+	public List<Transaction> getTransactions() {
+		return transactions;
+	}
+	public void setTransactions(List<Transaction> transactions) {
+		this.transactions = transactions;
+	}
+	
+	
+}
