c7e70fdbf703efabd98ea3ca85021736a6423250
mbcordeiro <matheusdebarroscordeiro@gmail.com>
2023-05-30 22:49:56 -0300

    Job config

---
+++ src/main/java/com/matheuscordeiro/creditcardbilljob/job/CreditCardBillJobConfig.java
@@ -0,0 +1,25 @@
+package com.matheuscordeiro.creditcardbilljob.job;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@EnableBatchProcessing
+@Configuration
+public class CreditCardBillJobConfig {
+	@Autowired
+	private JobBuilderFactory jobBuilderFactory;
+	
+	@Bean
+	public Job creditCardBillJob(Step creditCardBillStep) {
+		return jobBuilderFactory.get("creditCardBillStep")
+				.start(creditCardBillStep)
+				.incrementer(new RunIdIncrementer())
+				.build();
+	}
+}
